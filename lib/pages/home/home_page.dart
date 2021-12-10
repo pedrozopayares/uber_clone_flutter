88595fd81bdea2dc116dfcd1e81964d0ad1b9e7f
pedrozopayares <7519384+pedrozopayares@users.noreply.github.com>
2021-12-10 17:20:25 -0500

    Lección 13 - Organizar código del HomePage

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -19,69 +19,76 @@ class HomePage extends StatelessWidget {
             )
           ),
           child: Column(children: [
-            ClipPath(
-              clipper: DiagonalPathClipperTwo(),
-              child: Container(
-                color: Colors.white,
-                height: 200,
-                child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Image.asset(
-                        'assets/img/logo_app.png',
-                        width: 150,
-                        height: 100,
-                      ),
-                      Text(
-                          'Fácil y Rápido',
-                        style: TextStyle(
-                          fontFamily: 'Pacifico',
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ]
-                ),
-              ),
-            ),
+            _bannerApp(context),
             SizedBox(height:50),
-            Text(
-                'SELECCIONA TU ROL',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                  fontFamily: 'OneDay'
-                ),),
+            _selectYourRol(),
             SizedBox(height: 50,),
-            CircleAvatar(
-              backgroundImage: AssetImage('assets/img/pasajero.png'),
-              radius: 50,
-              backgroundColor: Colors.grey[900],
-            ),
+            _imageTypeUser('assets/img/pasajero.png'),
             SizedBox(height: 10,),
-            Text(
-                'Cliente',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
-            ),
+            _textTypeUser('Cliente'),
             SizedBox(height:30),
-            CircleAvatar(
-              backgroundImage: AssetImage('assets/img/driver.png'),
-              radius: 50,
-              backgroundColor: Colors.grey[900],
-            ),
+            _imageTypeUser('assets/img/driver.png'),
             SizedBox(height: 10,),
-            Text(
-                'Conductor',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20,
-                ),
-            ),
+            _textTypeUser('Conductor'),
           ]),
         ),
+      ),
+    );
+  }
+
+  Widget _bannerApp(BuildContext context) {
+    return ClipPath(
+      clipper: DiagonalPathClipperTwo(),
+      child: Container(
+        color: Colors.white,
+        height: 200,
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Image.asset(
+                'assets/img/logo_app.png',
+                width: 150,
+                height: 100,
+              ),
+              Text(
+                'Fácil y Rápido',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ]
+        ),
+      ),
+    );
+  }
+
+  Widget _selectYourRol() {
+    return Text(
+      'SELECCIONA TU ROL',
+      style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
+          fontFamily: 'OneDay'
+      ),
+    );
+  }
+
+  Widget _imageTypeUser(String image) {
+    return CircleAvatar(
+      backgroundImage: AssetImage(image),
+      radius: 50,
+      backgroundColor: Colors.grey[900],
+    );
+  }
+
+  Widget _textTypeUser(String typeUser) {
+    return Text(
+      typeUser,
+      style: TextStyle(
+        color: Colors.white,
+        fontSize: 20,
       ),
     );
   }
